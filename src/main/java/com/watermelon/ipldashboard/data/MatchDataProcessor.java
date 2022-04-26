8182c551bcec8196c889b3ac34dab40a14932b3d
Henry Derbyshire <henry@htderbyshire.com>
2022-04-26 10:32:32 +0100

    created model and processor for spring to take in csv data

---
+++ src/main/java/com/watermelon/ipldashboard/data/MatchDataProcessor.java
@@ -0,0 +1,45 @@
+package com.watermelon.ipldashboard.data;
+
+import com.watermelon.ipldashboard.model.Match;
+
+import org.springframework.batch.item.ItemProcessor;
+
+import java.time.LocalDate;
+
+public class MatchDataProcessor implements ItemProcessor<MatchInput, Match> {
+
+    @Override
+    public Match process(final MatchInput matchInput) throws Exception {
+        Match match = new Match();
+        match.setId(Long.parseLong(matchInput.getId()));
+        match.setCity(match.getCity());
+        match.setDate(LocalDate.parse(matchInput.getDate()));
+        match.setPlayerOfMatch(matchInput.getPlayer_of_match());
+
+        // Set team1 and team2 depending on the innings order.
+        String firstInningsTeam, secondInningsTeam;
+        if ("bat".equals(matchInput.getToss_decision())) {
+            firstInningsTeam = matchInput.getToss_winner();
+            secondInningsTeam = matchInput.getToss_decision().equals(matchInput.getTeam1()) ?
+                    matchInput.getTeam2() :
+                    matchInput.getTeam1();
+        } else {
+            secondInningsTeam = matchInput.getToss_winner();
+            firstInningsTeam = matchInput.getToss_decision().equals(matchInput.getTeam1()) ?
+                    matchInput.getTeam2() :
+                    matchInput.getTeam1();
+        }
+        match.setTeam1(firstInningsTeam);
+        match.setTeam2(secondInningsTeam);
+
+        match.setTossWinner(matchInput.getToss_winner());
+        match.setTossDecision(matchInput.getToss_decision());
+        match.setResult(matchInput.getResult());
+        match.setResultMargin(matchInput.getResult_margin());
+        match.setUmpire1(matchInput.getUmpire1());
+        match.setUmpire2(match.getUmpire2());
+
+        return match;
+    }
+
+}
