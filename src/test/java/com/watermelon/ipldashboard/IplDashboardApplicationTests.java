214177fc0dba0416bbccfe1ea5a85567486d0a55
Henry Derbyshire <henry@htderbyshire.com>
2022-04-26 09:58:14 +0100

    initial project setup, added spring dependencies

---
+++ src/test/java/com/watermelon/ipldashboard/IplDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.watermelon.ipldashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IplDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
