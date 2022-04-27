a82945627457e7c77b40fb984cb74ced9336c172
Henry Derbyshire <henry@htderbyshire.com>
2022-04-27 11:51:06 +0100

    created routes form team page and matches page

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -5,12 +5,14 @@ import {
     Routes
 } from "react-router-dom";
 import {TeamPage} from "./pages/TeamPage";
+import {MatchPage} from "./pages/MatchPage";
 
 function App() {
   return (
     <div className="App">
         <Router>
             <Routes>
+                <Route path="/teams/:teamName/matches/:year" element={<MatchPage />}/>
                 <Route path="/teams/:teamName" element={<TeamPage />}/>
             </Routes>
         </Router>
